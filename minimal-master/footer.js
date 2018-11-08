ad4f88d9dab169c11dc8b812c4a6bbd34dac4765
Monowar Hasan <mnwr.hsn@gmail.com>
2018-11-07 19:13:54 -0500

    Add files via upload

---
+++ minimal-master/footer.js
@@ -0,0 +1,6 @@
+document.write('\
+\
+    <p>This project is maintained by <a href="http://sibin-research.blogspot.com/" target="_blank">Sibin Mohan</a></p>\
+    <p><small>Hosted on GitHub Pages.</small></p>\
+\
+');
---
+++ minimal-master/header.js
@@ -0,0 +1,7 @@
+document.write('\
+\
+    <h2>SyNeRCyS Template 2018</h2>\
+    <h3>Texts</h4>\
+    <img src="./figs/f1.png" width="500">\
+\
+');
